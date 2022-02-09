8a7de0e60cad6efbc0385a46d6b66abf7855e690
George Abboud <gtabboud@gmail.com>
2022-02-09 14:09:21 -0500

    license header updates

--- cccalendar/force-app/main/default/lwc/cccalendar/cccalendar.js
+++ cccalendar/force-app/main/default/lwc/cccalendar/cccalendar.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { api, track } from 'lwc';
 import {loadStyle, loadScript} from 'lightning/platformResourceLoader';
 import fullCalendar from '@salesforce/resourceUrl/fullcalendar420';
